2b28dd971f9c8cf0881dd03f0b46cdcf34ed0b7c
Kyler Tucker <kmtucker@mix.wvu.edu>
2022-01-23 14:05:31 -0500

    added second navbar

---
+++ src/app/header/second.topnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: "fm-second-topnavbar",
+    templateUrl: "second.topnavbar.component.html.html",
+    styleUrls: ["second.topnavbar.component.css"]
+})
+export class SecondTopNavBarComponent {
+    
+}
