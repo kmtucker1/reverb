084e07e05c3abda73f2567d0f1cb0e2cdb754d03
Kyler Tucker <kmtucker@mix.wvu.edu>
2022-02-08 17:31:25 -0500

    Lab-3 commit

--- src/app/recent-view-row/mock_product_list.ts
+++ src/app/recent-view-row/mock_product_list.ts
@@ -1,38 +1,38 @@
 export const mock_product_list = [
     {
         img: "assets/recentView0.jpeg",
-        title: "asdf",
-        price: 12,
-        condition: "new"
+        title: "1953 Gibson ES-125",
+        price: 2300.00,
+        condition: "Used - Fair"
     },
     {
         img: "assets/recentView1.jpeg",
-        title: "asd",
-        price: 13,
-        condition: "used"
+        title: "Gibson Howard Roberts Custom 1974",
+        price: 5262.76,
+        condition: "Used - Good"
     },
     {
         img: "assets/recentView2.jpeg",
-        title: "sdf",
-        price: 14,
-        condition: "asdf"
+        title: "Fender USA American Vintage 52 Telecaster",
+        price: 1760.77,
+        condition: "Used - Very Good"
     },
     {
         img: "assets/recentView3.jpeg",
-        title: "zxcv",
-        price: 15,
-        condition: "used"
+        title: "Gibson ES-175 1954 Sunburst",
+        price: 6200.00,
+        condition: "Used - Good"
     },
     {
         img: "assets/recentView4.jpeg",
-        title: "qwer",
-        price: 16,
-        condition: "used"
+        title: "Gibson ES175 ES-175 1974",
+        price: 4395.00,
+        condition: "Used - Very Good"
     },
     {
         img: "assets/recentView5.jpeg",
-        title: "asdf",
-        price: 17,
-        condition: "used"
+        title: "1959 Gibson ES-175",
+        price: 12000.00,
+        condition: "Used - Good"
     }
 ];
