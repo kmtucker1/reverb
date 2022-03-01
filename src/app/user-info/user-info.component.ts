efef2bd53e184c1fdb70a228818fbd8cd41e53c0
Kyler Tucker <kmtucker@mix.wvu.edu>
2022-03-01 16:05:08 -0500

    Lab6

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ConnectableObservable } from 'rxjs';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 import { UserInfo } from './user-info.model';
 
 @Component({
@@ -8,7 +10,7 @@ import { UserInfo } from './user-info.model';
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
-  myInfo: UserInfo | undefined;
+ myInfo: UserInfo | undefined;
 
   constructor(private http: HttpClient) { }
 
@@ -18,12 +20,13 @@ export class UserInfoComponent implements OnInit {
   }
 
   getUserInfo() {
-    return this.http.get<UserInfo>('https://reverb-d1446-default-rtdb.firebaseio.com/.json');
+    return this.http.get<UserInfo>('https://reverb-d1446-default-rtdb.firebaseio.com/myinfo.json');
   }
   showUserInfo() {
     this.getUserInfo().subscribe((data: UserInfo) => {
       console.log(data);
       this.myInfo = data;
+      console.log(this.myInfo);
     })
   }
 }
